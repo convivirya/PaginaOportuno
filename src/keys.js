f9c2fcadf306b991efc3c31dd1c4fd3ffeb71b07
convivirya <conviveya205@gmail.com>
2023-04-22 18:37:12 -0500

    Commit

---
+++ src/keys.js
@@ -0,0 +1,8 @@
+module.exports={
+    database:{
+        host:'mysql-ecruz94.alwaysdata.net',
+        user:'ecruz94_oportuno',
+        password:'Liga1751532985',
+        database:'ecruz94_oportuno_inmobiliaria'
+    }
+};
